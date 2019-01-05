698742574b3876affd08a27ac92272e2294ffec1
Matt Bogen <matt@zebogen.com>
2019-01-05 17:56:04 -0500

    Add create-react-app

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>FilmBFF</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
